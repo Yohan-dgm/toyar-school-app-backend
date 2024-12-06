6f0f77c467159bd25baccf3a216acc3e16e1a1d5
Thanujitha Dilshan <thanujitha.dgm@gmail.com>
2024-12-06 16:14:45 +0530

    fix mistake UserManagement module

--- modules/UserManagement/Database/Sql/user_type.sql
+++ modules/UserManagement/Database/Sql/user_type.sql
@@ -55,4 +55,4 @@ ALTER TABLE "public"."user_type" ADD CONSTRAINT "user_type_pkey" PRIMARY KEY ("i
 -- ----------------------------
 ALTER SEQUENCE "public"."user_type_id_seq"
 OWNED BY "public"."user_type"."id";
-SELECT setval('"public"."user_type_id_seq"', 5, true);
+SELECT setval('"public"."user_type_id_seq"', 4, true);
