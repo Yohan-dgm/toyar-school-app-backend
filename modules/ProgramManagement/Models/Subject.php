ddccb2ab8a0ed8b4ef4b41940035e64ce1f4af3a
Thanujitha Dilshan <thanujitha.dgm@gmail.com>
2024-12-09 16:46:55 +0530

    update ProgramManagement module, update GetProgramListData intent

--- modules/ProgramManagement/Models/Subject.php
+++ modules/ProgramManagement/Models/Subject.php
@@ -5,6 +5,7 @@ namespace Modules\ProgramManagement\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Subject extends Model
 {
