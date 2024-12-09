9bd1c51723c40ad463f429c4f9a7c8eaff8be1a4
Thanujitha Dilshan <thanujitha.dgm@gmail.com>
2024-12-09 14:08:10 +0530

    update ProgramManagement module

---
+++ modules/ProgramManagement/Models/Stream.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\ProgramManagement\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Stream extends Model
+{
+    use HasFactory;
+
+    protected $table = 'stream';
+
+    protected $fillable = [
+        'name',
+        'created_by',
+        'updated_by',
+    ];
+
+    public $timestamps = true;
+
+    public function subject_list(): HasMany
+    {
+        return $this->hasMany(Program::class, 'stream_id', 'id');
+    }
+
+    // protected static function newFactory(): StreamFactory
+    // {
+    //     return new StreamFactory();
+    // }
+}
