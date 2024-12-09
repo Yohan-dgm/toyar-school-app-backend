b9d26368577bcca03b09b6b72d31217b65e04b99
Thanujitha Dilshan <thanujitha.dgm@gmail.com>
2024-12-09 22:50:49 +0530

    create CreateEducatorAttendance intent and create CreateStudentAttendance intent

--- modules/AttendanceManagement/routes.php
+++ modules/AttendanceManagement/routes.php
@@ -6,8 +6,12 @@ use Illuminate\Support\Facades\Route;
 use Modules\AttendanceManagement\Intents\EducatorAttendance\CreateEducatorAttendance\CreateEducatorAttendanceIntent;
 use Modules\AttendanceManagement\Intents\StudentAttendance\CreateStudentAttendance\CreateStudentAttendanceIntent;
 
-// Attendance
-Route::prefix('attendance')->group(function () {
-    Route::middleware('auth:web')->post('/create-educator-attendance', CreateEducatorAttendanceIntent::class)->name('attendance.create-educator-attendance');
-    Route::middleware('auth:web')->post('/create-student-attendance', CreateStudentAttendanceIntent::class)->name('attendance.create-student-attendance');
+// EducatorAttendance
+Route::prefix('educator-attendance')->group(function () {
+    Route::middleware('auth:web')->post('/create-educator-attendance', CreateEducatorAttendanceIntent::class)->name('educator-attendance.create-educator-attendance');
+});
+
+// StudentAttendance
+Route::prefix('student-attendance')->group(function () {
+    Route::middleware('auth:web')->post('/create-student-attendance', CreateStudentAttendanceIntent::class)->name('student-attendance.create-student-attendance');
 });
