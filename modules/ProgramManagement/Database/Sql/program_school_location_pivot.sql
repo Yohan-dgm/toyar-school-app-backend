d2162001745277c12fbdf4e1913ce9ed86ba8584
Thanujitha Dilshan <thanujitha.dgm@gmail.com>
2024-12-09 10:26:18 +0530

    update ProgramManagement module

--- modules/ProgramManagement/Database/Sql/program_school_location_pivot.sql
+++ modules/ProgramManagement/Database/Sql/program_school_location_pivot.sql
@@ -41,8 +41,23 @@ CREATE TABLE "public"."program_school_location_pivot" (
 ;
 
 -- ----------------------------
--- Records of program
+-- Records of program_school_location_pivot
 -- ----------------------------
+INSERT INTO "public"."program_school_location_pivot" VALUES (1, 1, NULL, '2024-12-09 10:30:29', NULL, 1);
+INSERT INTO "public"."program_school_location_pivot" VALUES (2, 1, NULL, '2024-12-09 10:30:29', NULL, 2);
+INSERT INTO "public"."program_school_location_pivot" VALUES (3, 1, NULL, '2024-12-09 10:30:29', NULL, 3);
+INSERT INTO "public"."program_school_location_pivot" VALUES (4, 1, NULL, '2024-12-09 10:30:29', NULL, 4);
+INSERT INTO "public"."program_school_location_pivot" VALUES (5, 1, NULL, '2024-12-09 10:30:29', NULL, 5);
+INSERT INTO "public"."program_school_location_pivot" VALUES (6, 1, NULL, '2024-12-09 10:30:29', NULL, 6);
+INSERT INTO "public"."program_school_location_pivot" VALUES (7, 1, NULL, '2024-12-09 10:30:29', NULL, 7);
+INSERT INTO "public"."program_school_location_pivot" VALUES (8, 1, NULL, '2024-12-09 10:30:29', NULL, 8);
+INSERT INTO "public"."program_school_location_pivot" VALUES (9, 1, NULL, '2024-12-09 10:30:29', NULL, 9);
+INSERT INTO "public"."program_school_location_pivot" VALUES (10, 1, NULL, '2024-12-09 10:30:29', NULL, 10);
+INSERT INTO "public"."program_school_location_pivot" VALUES (11, 1, NULL, '2024-12-09 10:30:29', NULL, 11);
+INSERT INTO "public"."program_school_location_pivot" VALUES (12, 1, NULL, '2024-12-09 10:30:29', NULL, 12);
+INSERT INTO "public"."program_school_location_pivot" VALUES (13, 1, NULL, '2024-12-09 10:30:29', NULL, 13);
+INSERT INTO "public"."program_school_location_pivot" VALUES (14, 1, NULL, '2024-12-09 10:30:29', NULL, 14);
+INSERT INTO "public"."program_school_location_pivot" VALUES (15, 1, NULL, '2024-12-09 10:30:29', NULL, 15);
 
 -- ----------------------------
 -- Primary Key structure for table program_school_location_pivot
@@ -54,4 +69,4 @@ ALTER TABLE "public"."program_school_location_pivot" ADD CONSTRAINT "program_sch
 -- ----------------------------
 ALTER SEQUENCE "public"."program_school_location_pivot_id_seq"
 OWNED BY "public"."program_school_location_pivot"."id";
-SELECT setval('"public"."program_school_location_pivot_id_seq"', 1, false);
+SELECT setval('"public"."program_school_location_pivot_id_seq"', 15, true);
