8938eba2c6c80afc6456470c0629ba2380281271
Thanujitha Dilshan <thanujitha.dgm@gmail.com>
2024-12-10 22:38:10 +0530

    update ProgramManagement module

--- modules/ProgramManagement/Models/GradeLevel.php
+++ modules/ProgramManagement/Models/GradeLevel.php
@@ -4,6 +4,7 @@ namespace Modules\ProgramManagement\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class GradeLevel extends Model
@@ -23,6 +24,11 @@ class GradeLevel extends Model
     public function program_list(): HasMany
     {
         return $this->hasMany(Program::class, 'grade_level_id', 'id');
+    }
+
+    public function grade_level_class(): BelongsTo
+    {
+        return $this->belongsTo(GradeLevelClass::class, 'grade_level_id', 'id');
     }
 
     // protected static function newFactory(): GradeLevelFactory
