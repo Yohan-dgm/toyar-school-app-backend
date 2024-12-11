aabce1bddeecb8b52c31296a3599b0ffaf8f51a4
Mithun (Team Lead) <itdepartment.dgm@gmail.com>
2024-12-11 14:34:45 +0530

    test git

--- modules/SystemEntityManagement/Database/Sql/system_default_9_tables.sql
+++ modules/SystemEntityManagement/Database/Sql/system_default_9_tables.sql
@@ -1,141 +1,139 @@
 /*
  Navicat Premium Data Transfer
-
+ 
  Source Server         : postgres_82.112.254.107_nexis_college_sms_staging
  Source Server Type    : PostgreSQL
  Source Server Version : 160006 (160006)
  Source Host           : 82.112.254.107:5432
  Source Catalog        : nexis_college_sms_staging
  Source Schema         : public
-
+ 
  Target Server Type    : PostgreSQL
  Target Server Version : 160006 (160006)
  File Encoding         : 65001
-
+ 
  Date: 11/12/2024 11:44:01
-*/
-
-
+ */
 -- ----------------------------
 -- Table structure for cache
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."cache";
+
 CREATE TABLE "public"."cache" (
-  "key" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "value" text COLLATE "pg_catalog"."default" NOT NULL,
-  "expiration" int4 NOT NULL
-)
-;
+    "key" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "value" text COLLATE "pg_catalog"."default" NOT NULL,
+    "expiration" int4 NOT NULL
+);
 
 -- ----------------------------
 -- Table structure for cache_locks
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."cache_locks";
+
 CREATE TABLE "public"."cache_locks" (
-  "key" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "owner" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "expiration" int4 NOT NULL
-)
-;
+    "key" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "owner" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "expiration" int4 NOT NULL
+);
 
 -- ----------------------------
 -- Table structure for failed_jobs
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."failed_jobs";
+
 CREATE TABLE "public"."failed_jobs" (
-  "id" int8 NOT NULL DEFAULT nextval('failed_jobs_id_seq'::regclass),
-  "uuid" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "connection" text COLLATE "pg_catalog"."default" NOT NULL,
-  "queue" text COLLATE "pg_catalog"."default" NOT NULL,
-  "payload" text COLLATE "pg_catalog"."default" NOT NULL,
-  "exception" text COLLATE "pg_catalog"."default" NOT NULL,
-  "failed_at" timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
-;
+    "id" int8 NOT NULL DEFAULT nextval('failed_jobs_id_seq' :: regclass),
+    "uuid" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "connection" text COLLATE "pg_catalog"."default" NOT NULL,
+    "queue" text COLLATE "pg_catalog"."default" NOT NULL,
+    "payload" text COLLATE "pg_catalog"."default" NOT NULL,
+    "exception" text COLLATE "pg_catalog"."default" NOT NULL,
+    "failed_at" timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
 -- ----------------------------
 -- Table structure for job_batches
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."job_batches";
+
 CREATE TABLE "public"."job_batches" (
-  "id" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "total_jobs" int4 NOT NULL,
-  "pending_jobs" int4 NOT NULL,
-  "failed_jobs" int4 NOT NULL,
-  "failed_job_ids" text COLLATE "pg_catalog"."default" NOT NULL,
-  "options" text COLLATE "pg_catalog"."default",
-  "cancelled_at" int4,
-  "created_at" int4 NOT NULL,
-  "finished_at" int4
-)
-;
+    "id" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "total_jobs" int4 NOT NULL,
+    "pending_jobs" int4 NOT NULL,
+    "failed_jobs" int4 NOT NULL,
+    "failed_job_ids" text COLLATE "pg_catalog"."default" NOT NULL,
+    "options" text COLLATE "pg_catalog"."default",
+    "cancelled_at" int4,
+    "created_at" int4 NOT NULL,
+    "finished_at" int4
+);
 
 -- ----------------------------
 -- Table structure for jobs
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."jobs";
+
 CREATE TABLE "public"."jobs" (
-  "id" int8 NOT NULL DEFAULT nextval('jobs_id_seq'::regclass),
-  "queue" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "payload" text COLLATE "pg_catalog"."default" NOT NULL,
-  "attempts" int2 NOT NULL,
-  "reserved_at" int4,
-  "available_at" int4 NOT NULL,
-  "created_at" int4 NOT NULL
-)
-;
+    "id" int8 NOT NULL DEFAULT nextval('jobs_id_seq' :: regclass),
+    "queue" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "payload" text COLLATE "pg_catalog"."default" NOT NULL,
+    "attempts" int2 NOT NULL,
+    "reserved_at" int4,
+    "available_at" int4 NOT NULL,
+    "created_at" int4 NOT NULL
+);
 
 -- ----------------------------
 -- Table structure for migrations
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."migrations";
+
 CREATE TABLE "public"."migrations" (
-  "id" int4 NOT NULL DEFAULT nextval('migrations_id_seq'::regclass),
-  "migration" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "batch" int4 NOT NULL
-)
-;
+    "id" int4 NOT NULL DEFAULT nextval('migrations_id_seq' :: regclass),
+    "migration" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "batch" int4 NOT NULL
+);
 
 -- ----------------------------
 -- Table structure for password_reset_tokens
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."password_reset_tokens";
+
 CREATE TABLE "public"."password_reset_tokens" (
-  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "token" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "created_at" timestamp(0)
-)
-;
+    "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "token" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "created_at" timestamp(0)
+);
 
 -- ----------------------------
 -- Table structure for personal_access_tokens
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."personal_access_tokens";
+
 CREATE TABLE "public"."personal_access_tokens" (
-  "id" int8 NOT NULL DEFAULT nextval('personal_access_tokens_id_seq'::regclass),
-  "tokenable_type" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "tokenable_id" int8 NOT NULL,
-  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "token" varchar(64) COLLATE "pg_catalog"."default" NOT NULL,
-  "abilities" text COLLATE "pg_catalog"."default",
-  "last_used_at" timestamp(0),
-  "expires_at" timestamp(0),
-  "created_at" timestamp(0),
-  "updated_at" timestamp(0)
-)
-;
+    "id" int8 NOT NULL DEFAULT nextval('personal_access_tokens_id_seq' :: regclass),
+    "tokenable_type" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "tokenable_id" int8 NOT NULL,
+    "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "token" varchar(64) COLLATE "pg_catalog"."default" NOT NULL,
+    "abilities" text COLLATE "pg_catalog"."default",
+    "last_used_at" timestamp(0),
+    "expires_at" timestamp(0),
+    "created_at" timestamp(0),
+    "updated_at" timestamp(0)
+);
 
 -- ----------------------------
 -- Table structure for sessions
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."sessions";
+
 CREATE TABLE "public"."sessions" (
-  "id" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
-  "user_id" int8,
-  "ip_address" varchar(45) COLLATE "pg_catalog"."default",
-  "user_agent" text COLLATE "pg_catalog"."default",
-  "payload" text COLLATE "pg_catalog"."default" NOT NULL,
-  "last_activity" int4 NOT NULL
-)
-;
+    "id" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+    "user_id" int8,
+    "ip_address" varchar(45) COLLATE "pg_catalog"."default",
+    "user_agent" text COLLATE "pg_catalog"."default",
+    "payload" text COLLATE "pg_catalog"."default" NOT NULL,
+    "last_activity" int4 NOT NULL
+);
