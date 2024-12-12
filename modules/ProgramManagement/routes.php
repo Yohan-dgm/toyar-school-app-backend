7d432b1e8feb4d4d840b3e48f9c81fed8efddf8a
Thanujitha Dilshan <thanujitha.dgm@gmail.com>
2024-12-13 00:28:54 +0530

    edit ProgramManagement routes

--- modules/ProgramManagement/routes.php
+++ modules/ProgramManagement/routes.php
@@ -7,5 +7,8 @@ use Modules\ProgramManagement\Intents\ProgramManagement\AttachSubjectListToEduca
 // Program
 Route::prefix('program')->group(function () {
     Route::middleware('auth:web')->post('/get-program-list-data', GetProgramListDataIntent::class)->name('program.get-program-list-data');
-    Route::middleware('auth:web')->post('/attach-subject-list-to-educator', AttachSubjectListToEducatorIntent::class)->name('program.attach-subject-list-to-educator');
+});
+// program-management
+Route::prefix('program-management')->group(function () {
+    Route::middleware('auth:web')->post('/attach-subject-list-to-educator', AttachSubjectListToEducatorIntent::class)->name('program-management.attach-subject-list-to-educator');
 });
